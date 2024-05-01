8f548b88072b372f7180ac3e52d4c54bd3911e1b
binzcodes <r@rbi.nz>
2024-05-02 05:50:18 +0800

    fix(pkg): failing to import

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
 // @ts-ignore Head.astro is untyped unless loaded by language tools
-export { default as Visor } from './src/components/Head.astro';
-export type {
-  Author,
-  TwitterHandle,
-} from './src/components/Head.astro';
+export { default as Visor, type Author, type TwitterHandle } from './src/components/Head.astro';
