aa520e3f4ab0411cf1bbcfd5caea311fda3fbaa0
binzcodes <r@rbi.nz>
2024-05-02 03:28:01 +0800

    tmp: stash 1

---
+++ index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore Head.astro is untyped unless loaded by language tools
+export { default as Visor } from './src/components/Head.astro';
+export type {
+  Author,
+  TwitterHandle,
+} from './src/components/Head.astro';
