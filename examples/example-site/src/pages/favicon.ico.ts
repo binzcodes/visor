5057fbce715730ab2655d70978b487c8b038d54e
binzcodes <r@rbi.nz>
2024-10-25 16:44:59 +0100

    refactor: use astro integration standards

--- examples/example-site/src/pages/favicon.ico.ts
+++ examples/example-site/src/pages/favicon.ico.ts
@@ -1,6 +1,6 @@
-import type {APIRoute} from "astro";
-import Favicon from "@binz/visor/src/lib/favicon";
 import path from "node:path";
+import type {APIRoute} from "astro";
+import Favicon from "@binz/visor/favicon";
 
 const faviconSrc = path.resolve("src/images/Logo.svg");
 
--- packages/visor/example-pages/favicon.ico.ts
+++
@@ -1,7 +0,0 @@
-import type {APIRoute} from "astro";
-import Favicon from "../src/lib/favicon";
-import path from "node:path";
-
-const faviconSrc = path.resolve("src/images/Logo.svg");
-
-export const GET: APIRoute = Favicon({faviconSrc});
