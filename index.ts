76aff75adc7461afc2df4863085064ea7d8f871c
binzcodes <r@rbi.nz>
2024-10-25 10:44:22 +0100

    chore: lint

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
 // @ts-ignore Head.astro is untyped unless loaded by language tools
-export { default as Visor, type Author, type TwitterHandle } from './src/components/Head.astro';
+export {
+  default as Visor,
+  type Author,
+  type TwitterHandle,
+} from "./src/components/Head.astro";
